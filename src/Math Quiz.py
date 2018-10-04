4122ff8cb611b083aaf61f0739e73b05bc8c233b
Michael Trinh <mptrinh@cpp.edu>
2018-10-03 19:18:34 -0700

    optimized code

--- src/Math Quiz.py
+++ src/Math Quiz.py
@@ -1,36 +1,39 @@
 # Michael Trinh
 # CS 3750
-# Python Program
+# Python Program : Math Quiz
 
-import random
+import random, math
 
-# list of operations and expression
-listOperations = ['+','-','x','/']
-listExpression = []
+# list of operators and expression
+listOperators = ['+','-','x','/','**']
+# mathematical expression
+expression = ''
 
 # gets the mathematical expression in list form
-def getExpression(listOperations):
-    # number of operands here, at least 2
+def getExpression(listOperators):
+    # list output of expression
+    listExpression = []
+    # number of operands in expression, more than 2 operands adds complication
+    # in calculating expression from string 
     operands = 2
-    print('Starting Test...')
     # create an expression in list form using a loop
     for num in range(0,operands):
         # add operand to the list
-        listExpression.append(random.randint(1,10))
+        listExpression.append(random.randint(0,10))
         # add an operator after all operands except last one
         if(num != (operands-1)):
-            listExpression.append(random.choice(listOperations))
+            listExpression.append(random.choice(listOperators))
+    return listExpression        
 # end getExpression
 
 # prints the question
-def printQuestion(list):
+def printQuestion(list, expression):
+    # converts the expression from list form to string form
     for element in list:
-        if(element == list[0]):
-            expression = str(element)
-        else:
-            expression += str(element)
+        expression += str(element)
+    # prints question    
     print('What is ' + expression + ' ?')
-# end pringQuestion
+# end printQuestion
 
-getExpression(listOperations)
-printQuestion(listExpression)
+printQuestion(getExpression(listOperators), expression)
+
